5b73cfe299a364a4ac8e0bfad79dde878957ab55
David <dsarnavskiy77@gmail.com>
2021-05-23 21:25:43 +0300

    adduniversalcommand

--- main.py
+++ main.py
@@ -15,17 +15,15 @@ SendKits = {}
 def IsSelf(message):
     if message.from_user.is_self:
         return True
-    RepliedUser = app.get_messages(message.chat.id, reply_to_message_ids=message.message_id).from_user
-    if RepliedUser.is_self:
+    RepliedUser = app.get_messages(message.chat.id, reply_to_message_ids=message.message_id)
+    if RepliedUser.from_user.is_self:
         return True
     else:
         return False
 
-def IsAll(message, command):
+def IsAll(message):
     sleep(random.randrange(1, 20, 1)/10)
-    text = "None"
-    if not IsSelf(message):
-        text = message.text.split(command, maxsplit=1)[1]
+    text = message.text.split(maxsplit=2)[1]
 
     if text == "all":
         return True
@@ -34,7 +32,23 @@ def IsAll(message, command):
 
 @app.on_message(filters.text & filters.command("status", prefixes="."))
 def Status(client, message):
-    if IsAll(message, ".status ") or IsSelf(message):
+    if IsSelf(message):
+        message.delete()
+        try:
+            IsWorking = Works.get(message.chat.id).IsWorking
+        except:
+            IsWorking = False
+
+        try:
+            IsEating = Eats.get(message.chat.id).IsEating
+        except:
+            IsEating = False
+
+        try:
+            IsSendingKits = SendKits.get(message.chat.id).IsSendingKits
+        except:
+            IsSendingKits = False
+    elif IsAll(message):
         message.delete()
         try:
             IsWorking = Works.get(message.chat.id).IsWorking
@@ -51,9 +65,9 @@ def Status(client, message):
         except:
             IsSendingKits = False
 
-        message.reply_text(f"IsEating = {IsEating}\n"
-                           f"IsWorking = {IsWorking}\n"
-                           f"IsSendingKits = {IsSendingKits}")
+    message.reply_text(f"IsEating = {IsEating}\n"
+                        f"IsWorking = {IsWorking}\n"
+                        f"IsSendingKits = {IsSendingKits}")
 
 
 @app.on_message(filters.command("send", prefixes="."))
@@ -128,53 +142,13 @@ def WorkCommand(client, message):
         else:
             message.reply_text("Unknown command")
 
-@app.on_message(filters.text & filters.command("take", prefixes="."))
-def TakeFrog(client, message):
-    if IsSelf(message):
-        message.delete()
-        message.reply_text("Взять жабу", quote=False)
+@app.on_message(filters.text & filters.command("repeat", prefixes="."))
+def Repeat(client, message):
+    if IsAll(message, ".repeat "):
+        message.reply_text(message.text.split(maxsplit=2)[2], quote=True)
+    elif IsSelf(message):
+        message.reply_text(message.text.split(maxsplit=1)[1], quote=True)
 
-@app.on_message(filters.text & filters.command("class", prefixes="."))
-def TakeClass(client, message):
-    if IsSelf(message):
-        message.delete()
-        message.reply_text("Выбрать класс Авантюрист", quote=False)
-
-@app.on_message(filters.text & filters.command("inventory", prefixes="."))
-def Inventory(client, message):
-    if IsAll(message, ".inventory ") or IsSelf(message):
-        message.delete()
-        message.reply_text("Мой инвентарь", quote=False)
-
-@app.on_message(filters.text & filters.command("balance", prefixes="."))
-def Balance(client, message):
-    if IsAll(message, ".balance ") or IsSelf(message):
-        message.delete()
-        message.reply_text("Мой баланс", quote=False)
-
-@app.on_message(filters.text & filters.command("myfrog", prefixes="."))
-def Frog(client, message):
-    if IsAll(message, ".myfrog ") or IsSelf(message):
-        message.delete()
-        message.reply_text("Моя жаба", quote=False)
-
-@app.on_message(filters.text & filters.command("froginfo", prefixes="."))
-def Info(client, message):
-    if IsAll(message, ".froginfo ") or IsSelf(message):
-        message.delete()
-        message.reply_text("Жаба инфо", quote=False)
-
-@app.on_message(filters.text & filters.command("sendmoney", prefixes="."))
-def SendMoney(client, message):
-    if IsSelf(message):
-        count = int(message.text.split(".sendmoney ", maxsplit=1)[1])
-        app.send_message(message.chat.id, f"Отправить букашки {count}", reply_to_message_id=message.message_id)
-
-@app.on_message(filters.text & filters.command("name", prefixes="."))
-def Name(client, message):
-    if IsSelf(message):
-        name = message.text.split(".name ", maxsplit=1)[1]
-        message.reply_text(f"Дать жабе имя {name}", quote=False)
 
 
 @app.on_message(filters.text & filters.command("help", prefixes="."))
@@ -185,13 +159,6 @@ def Help(client, message):
                            f".send [start/stop]\n"
                            f".eat [name of work/stop]\n"
                            f".work [name of work/stop]\n"
-                           f".take (to take frog)\n"
-                           f".class (to take the class)\n"
-                           f".inventory [all/(repeat)]\n"
-                           f".balance [all/(repeat)]\n"
-                           f".myfrog [all/(repeat)]\n"
-                           f".froginfo all/(repeat)]\n"
-                           f".sendmoney [Number]\n"
-                           f".name (set name)")
+                           f".repeat [all message/ message]")
 
 app.run()
